93fea78ed8d0d27fbec381d6cb85c3aad22798c8
HashiCoop <cooper@hashicorp.com>
2023-01-25 13:20:19 -0600

    use single required providers block

--- gopher-consul/main.tf
+++ gopher-consul/main.tf
@@ -1,11 +1,21 @@
 terraform {
   required_providers {
+    doormat = {
+      source  = "doormat.hashicorp.services/hashicorp-security/doormat"
+      version = "~> 0.0.0"
+    }
+
     helm = {
       source  = "hashicorp/helm"
       version = "2.5.1"
     }
   }
 }
+
+data "doormat_aws_credentials" "creds" {
+  role_arn = "arn:aws:iam::390101570318:role/${var.TFC_WORKSPACE_NAME}"
+}
+
 
 provider "aws" {
   region     = "us-east-1"
