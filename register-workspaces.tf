432ad175128a6b1e03b6f4ef249c335812a1405b
HashiCoop <cooper@hashicorp.com>
2023-01-24 23:46:07 -0600

    add vars

--- register-workspaces.tf
+++ register-workspaces.tf
@@ -1,4 +1,10 @@
 module "auto-register-monorepo-workspaces" {
   source  = "app.terraform.io/hashicoop/auto-register-monorepo-workspaces/tfc"
   version = "0.1.0"
+
+  TFC_WORKSPACE_NAME = var.TFC_WORKSPACE_NAME
 }
+
+variable "TFC_WORKSPACE_NAME" {}
+
+variable "OAUTH_CLIENT_ID" {}
