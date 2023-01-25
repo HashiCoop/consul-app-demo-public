798d67c477b829f894159912c1f07bffd28cb040
HashiCoop <cooper@hashicorp.com>
2023-01-24 23:56:12 -0600

    use latest module

--- register-workspaces.tf
+++ register-workspaces.tf
@@ -1,6 +1,5 @@
 module "auto-register-monorepo-workspaces" {
   source  = "app.terraform.io/hashicoop/auto-register-monorepo-workspaces/tfc"
-  version = "0.1.0"
 
   TFC_WORKSPACE_NAME = var.TFC_WORKSPACE_NAME
   OAUTH_CLIENT_ID = var.OAUTH_CLIENT_ID
