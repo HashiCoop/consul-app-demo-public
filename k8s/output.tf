98bc53295828c71d133bbc2276b8b1616edbafa4
HashiCoop <cooper@hashicorp.com>
2023-01-25 13:42:59 -0600

    all outputs are already non-sensitive

--- k8s/output.tf
+++ k8s/output.tf
@@ -3,7 +3,7 @@ output "cluster_endpoint" {
 }
 
 output "cluster_certificate_authority_data" {
-    value = nonsensitive(module.eks.cluster_certificate_authority_data)
+    value = module.eks.cluster_certificate_authority_data
 }
 
 output "cluster_id" {
