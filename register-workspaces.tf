9f13726b6c5f7194ae4cfe680fb69148cd6943cc
HashiCoop <cooper@hashicorp.com>
2023-01-24 23:46:48 -0600

    pass throug vars

--- register-workspaces.tf
+++ register-workspaces.tf
@@ -3,6 +3,7 @@ module "auto-register-monorepo-workspaces" {
   version = "0.1.0"
 
   TFC_WORKSPACE_NAME = var.TFC_WORKSPACE_NAME
+  OAUTH_CLIENT_ID = var.OAUTH_CLIENT_ID
 }
 
 variable "TFC_WORKSPACE_NAME" {}
