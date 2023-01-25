23e67b3c229f1ead27e25a8005f3c263afef5dc5
HashiCoop <cooper@hashicorp.com>
2023-01-25 13:30:00 -0600

    test cluster outputs

--- gopher-consul/main.tf
+++ gopher-consul/main.tf
@@ -16,7 +16,6 @@ data "doormat_aws_credentials" "creds" {
   role_arn = "arn:aws:iam::390101570318:role/${var.TFC_WORKSPACE_NAME}"
 }
 
-
 provider "aws" {
   region     = "us-east-1"
   access_key = data.doormat_aws_credentials.creds.access_key
@@ -29,35 +28,39 @@ data "tfe_outputs" "cluster" {
   workspace = var.TFC_CLUSTER_WORKSPACE
 }
 
-data "aws_eks_cluster" "cluster" {
-  name = data.tfe_outputs.cluster.values.cluster_id
+output "cluster" {
+    value = data.tfe_outputs.cluster.values
 }
 
-data "aws_eks_cluster_auth" "cluster" {
-  name = data.tfe_outputs.cluster.values.cluster_id
-}
+# data "aws_eks_cluster" "cluster" {
+#   name = data.tfe_outputs.cluster.values.cluster_id
+# }
 
-# provider "kubernetes" {
+# data "aws_eks_cluster_auth" "cluster" {
+#   name = data.tfe_outputs.cluster.values.cluster_id
+# }
+
+# # provider "kubernetes" {
+# #   host                   = data.aws_eks_cluster.cluster.endpoint
+# #   token                  = data.aws_eks_cluster_auth.cluster.token
+# #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+# # }
+
+# provider "helm" {
+#   kubernetes {
 #   host                   = data.aws_eks_cluster.cluster.endpoint
 #   token                  = data.aws_eks_cluster_auth.cluster.token
 #   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+#   }
 # }
 
-provider "helm" {
-  kubernetes {
-  host                   = data.aws_eks_cluster.cluster.endpoint
-  token                  = data.aws_eks_cluster_auth.cluster.token
-  cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
-  }
-}
+# resource "helm_release" "consul" {
+#   name       = "consul"
 
-resource "helm_release" "consul" {
-  name       = "consul"
+#   repository = "https://helm.releases.hashicorp.com"
+#   chart      = "consul"
 
-  repository = "https://helm.releases.hashicorp.com"
-  chart      = "consul"
-
-  values = [
-    "${file("config/consul-values.yaml")}"
-  ]
-}
+#   values = [
+#     "${file("config/consul-values.yaml")}"
+#   ]
+# }
