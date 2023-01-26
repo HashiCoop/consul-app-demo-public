704fa9231071b4fb42755ee5cfe254f82096800e
HashiCoop <cooper@hashicorp.com>
2023-01-26 16:21:40 -0600

    register external vault service (pub ip for test)

---
+++ gopher-consul/vault.tf
@@ -0,0 +1,25 @@
+resource "consul_service" "vault" {
+    provider = consul.aws
+
+    name = "vault"
+    node = "vault"
+    address = "https://vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud"
+    port = 8200
+    meta = {
+        external-node = "true"
+        external-probe = "false"
+    }
+
+    tags = ["v1"]
+
+    check {
+        check_id                          = "service:vault_api"
+        name                              = "vault health"
+        status                            = "passing"
+        http                              = "https://https://vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud:8200/v1/sys/health"
+        tls_skip_verify                   = false
+        interval                          = "10s"
+        timeout                           = "5s"
+        deregister_critical_service_after = "30s"
+    }
+}
