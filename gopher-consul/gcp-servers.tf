4a8a1cde2d536eb83d1ac9043f8526b5fbbe9f48
HashiCoop <cooper@hashicorp.com>
2023-01-25 20:11:54 -0600

    use gcp creds

---
+++ gopher-consul/gcp-servers.tf
@@ -0,0 +1,20 @@
+provider "google" {
+  project     = "cmelgreen-consul-mesh-test"
+  region      = "us-central1"
+  zone        = "us-central1-c"
+}
+
+data "google_client_config" "provider" {}
+
+data "google_container_cluster" "my_cluster" {
+  name     = "consul-autopilot"
+  location = "us-central1"
+}
+
+provider "kubernetes" {
+  host  = "https://${data.google_container_cluster.my_cluster.endpoint}"
+  token = data.google_client_config.provider.access_token
+  cluster_ca_certificate = base64decode(
+    data.google_container_cluster.my_cluster.master_auth[0].cluster_ca_certificate,
+  )
+}
