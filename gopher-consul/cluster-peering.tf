747640ae54d9d9eb0dba9f13ba4a82e45bbaf707
HashiCoop <cooper@hashicorp.com>
2023-01-26 13:24:51 -0600

    remove peering

--- gopher-consul/cluster-peering.tf
+++ gopher-consul/cluster-peering.tf
@@ -14,35 +14,37 @@ provider "consul" {
   insecure_https = true
 }
 
-resource "kubernetes_manifest" "mesh_gateway" {
-  manifest  = yamldecode(file("./config/mesh-gw.yaml"))
-}
+# resource "kubernetes_manifest" "mesh_gateway" {
+#   manifest  = yamldecode(file("./config/mesh-gw.yaml"))
+#   namespace = "consul"
+# }
 
-resource "kubernetes_manifest" "gcp_mesh_gateway" {
-  provider  = kubernetes.gcp
-  manifest  = yamldecode(file("./config/mesh-gw.yaml"))
-}
+# resource "kubernetes_manifest" "gcp_mesh_gateway" {
+#   provider  = kubernetes.gcp
+#   manifest  = yamldecode(file("./config/mesh-gw.yaml"))
+#   namespace = "consul"
+# }
 
-resource "consul_peering_token" "aws-gcp" {
-  provider  = consul.aws
-  peer_name = "gcp-cluster"
+# resource "consul_peering_token" "aws-gcp" {
+#   provider  = consul.aws
+#   peer_name = "gcp-cluster"
 
-  depends_on = [
-    kubernetes_manifest.gcp_mesh_gateway
-  ]
-}
+#   depends_on = [
+#     kubernetes_manifest.gcp_mesh_gateway
+#   ]
+# }
 
-resource "consul_peering" "gcp-aws" {
-  provider = consul.gcp
+# resource "consul_peering" "gcp-aws" {
+#   provider = consul.gcp
 
-  peer_name     = "aws-cluster"
-  peering_token = consul_peering_token.aws-gcp.peering_token
+#   peer_name     = "aws-cluster"
+#   peering_token = consul_peering_token.aws-gcp.peering_token
 
-  meta = {
-    hello = "world"
-  }
+#   meta = {
+#     hello = "world"
+#   }
 
-  depends_on = [
-    kubernetes_manifest.gcp_mesh_gateway
-  ]
-}
+#   depends_on = [
+#     kubernetes_manifest.gcp_mesh_gateway
+#   ]
+# }
