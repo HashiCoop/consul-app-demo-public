2128ff421509518380229c33d64f0212c469ae2e
HashiCoop <cooper@hashicorp.com>
2023-01-25 23:04:13 -0600

    fix consul bootstrap token reference

--- gopher-consul/cluster-peering.tf
+++ gopher-consul/cluster-peering.tf
@@ -1,13 +1,13 @@
 provider "consul" {
   alias   = "aws"
   address = data.kubernetes_service.consul_ui.status[0].load_balancer[0].ingress[0].hostname
-  token = data.kubernetes_secret.consul_bootstrap_acl_token.data
+  token = data.kubernetes_secret.consul_bootstrap_acl_token.data.token
 }
 
 provider "consul" {
   alias   = "gcp"
   address = data.kubernetes_service.gcp_consul_ui.status[0].load_balancer[0].ingress[0].ip
-  token = data.kubernetes_secret.gcp_consul_bootstrap_acl_token.data
+  token = data.kubernetes_secret.gcp_consul_bootstrap_acl_token.data.token
 }
 
 resource "consul_peering_token" "aws-gcp" {
