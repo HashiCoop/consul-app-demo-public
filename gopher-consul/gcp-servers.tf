b5b4a96d32c04619af97d523d581e904389c2fbb
HashiCoop <cooper@hashicorp.com>
2023-01-25 22:34:10 -0600

    leave providers but still remove gcp consul

--- gopher-consul/gcp-servers.tf
+++ gopher-consul/gcp-servers.tf
@@ -25,15 +25,15 @@ provider "kubernetes" {
 #   }
 # }
 
-# provider "helm" {
-#   kubernetes {
-#     host  = "https://${data.google_container_cluster.cluster.endpoint}"
-#     token = data.google_client_config.provider.access_token
-#     cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
-#   }
+provider "helm" {
+  kubernetes {
+    host  = "https://${data.google_container_cluster.cluster.endpoint}"
+    token = data.google_client_config.provider.access_token
+    cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+  }
 
-#   alias = "gcp"
-# }
+  alias = "gcp"
+}
 
 # resource "helm_release" "gcp_consul" {
 #   provider = helm.gcp
