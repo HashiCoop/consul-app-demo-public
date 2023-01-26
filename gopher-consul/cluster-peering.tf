6031593efd72028567ed9a2bbfbdc3d33dbc972a
HashiCoop <cooper@hashicorp.com>
2023-01-26 15:28:38 -0600

    peer clusters

--- gopher-consul/cluster-peering.tf
+++ gopher-consul/cluster-peering.tf
@@ -8,8 +8,8 @@ variable "GCP_CONSUL_TOKEN" {
 
 provider "consul" {
   alias          = "aws"
-  address        = "a9e78ecc71269441da66304cd477764d-1559337923.us-east-1.elb.amazonaws.com"
-  token          = var.AWS_CONSUL_TOKEN
+  address        = data.kubernetes_service.consul_ui.status[0].load_balancer[0].ingress[0].hostname
+  token          = data.kubernetes_secret.consul_bootstrap_acl_token.data.token
   scheme         = "https"
   insecure_https = true
 }
@@ -21,10 +21,10 @@ resource "consul_peering_token" "aws-gcp" {
 
 provider "consul" {
   alias          = "gcp"
-  #address        = data.kubernetes_service.gcp_consul_ui.status[0].load_balancer[0].ingress[0].ip
-  #token          = data.kubernetes_secret.gcp_consul_bootstrap_acl_token.data.token
-  address       = "34.136.236.23"
-  token = var.GCP_CONSUL_TOKEN
+  address        = data.kubernetes_service.gcp_consul_ui.status[0].load_balancer[0].ingress[0].ip
+  token          = data.kubernetes_secret.gcp_consul_bootstrap_acl_token.data.token
+#   address       = "34.136.236.23"
+#   token = var.GCP_CONSUL_TOKEN
   scheme         = "https"
   insecure_https = true
 }
@@ -33,27 +33,27 @@ output "gcp_consul_api_listed_endpoint" {
     value = data.kubernetes_service.gcp_consul_ui.status[0].load_balancer[0].ingress[0].ip
 }
 
-# resource "kubernetes_manifest" "mesh_gateway" {
-#   manifest  = yamldecode(file("./config/mesh-gw.yaml"))
+resource "kubernetes_manifest" "mesh_gateway" {
+  manifest  = yamldecode(file("./config/mesh-gw.yaml"))
 
-# }
+}
 
-# resource "kubernetes_manifest" "gcp_mesh_gateway" {
-#   provider  = kubernetes.gcp
-#   manifest  = yamldecode(file("./config/mesh-gw.yaml"))
-# }
+resource "kubernetes_manifest" "gcp_mesh_gateway" {
+  provider  = kubernetes.gcp
+  manifest  = yamldecode(file("./config/mesh-gw.yaml"))
+}
 
-# resource "consul_peering" "gcp-aws" {
-#   provider = consul.gcp
+resource "consul_peering" "gcp-aws" {
+  provider = consul.gcp
 
-#   peer_name     = "aws-cluster"
-#   peering_token = consul_peering_token.aws-gcp.peering_token
+  peer_name     = "aws-cluster"
+  peering_token = consul_peering_token.aws-gcp.peering_token
 
-#   meta = {
-#     hello = "world"
-#   }
+  meta = {
+    hello = "world"
+  }
 
-#   depends_on = [
-#     kubernetes_manifest.gcp_mesh_gateway
-#   ]
-# }
+  depends_on = [
+    kubernetes_manifest.gcp_mesh_gateway
+  ]
+}
