87aa00a5f332b30aefc584e16df14b1477abb754
HashiCoop <cooper@hashicorp.com>
2023-01-25 23:33:28 -0600

    namespace

--- gopher-consul/cluster-peering.tf
+++ gopher-consul/cluster-peering.tf
@@ -2,7 +2,7 @@ provider "consul" {
   alias          = "aws"
   address        = data.kubernetes_service.consul_ui.status[0].load_balancer[0].ingress[0].hostname
   token          = data.kubernetes_secret.consul_bootstrap_acl_token.data.token
-  scheme ="https"
+  scheme         = "https"
   insecure_https = true
 }
 
@@ -10,17 +10,19 @@ provider "consul" {
   alias          = "gcp"
   address        = data.kubernetes_service.gcp_consul_ui.status[0].load_balancer[0].ingress[0].ip
   token          = data.kubernetes_secret.gcp_consul_bootstrap_acl_token.data.token
-  scheme ="https"
+  scheme         = "https"
   insecure_https = true
 }
 
 resource "kubernetes_manifest" "mesh_gateway" {
-    manifest = yamldecode(file("./config/mesh-gw.yaml"))
+  manifest  = yamldecode(file("./config/mesh-gw.yaml"))
+  namespace = "consul"
 }
 
 resource "kubernetes_manifest" "gcp_mesh_gateway" {
-    provider = kubernetes.gcp
-    manifest = yamldecode(file("./config/mesh-gw.yaml"))
+  provider  = kubernetes.gcp
+  manifest  = yamldecode(file("./config/mesh-gw.yaml"))
+  namespace = "consul"
 }
 
 resource "consul_peering_token" "aws-gcp" {
