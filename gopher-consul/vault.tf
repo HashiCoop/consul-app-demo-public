20aad576e4db7c0bd6e612d3f5bd8c8ad3d0b7a3
HashiCoop <cooper@hashicorp.com>
2023-01-27 08:45:50 -0600

    deploy external vault service

--- gopher-consul/vault.tf
+++ gopher-consul/vault.tf
@@ -4,28 +4,28 @@ resource "consul_node" "vault" {
   address = "https://vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud"
 }
 
-# resource "consul_service" "vault" {
-#     provider = consul.aws
+resource "consul_service" "vault" {
+    provider = consul.aws
 
-#     name = "vault"
-#     node = resource.consul_node.vault.name
-#     port = 8200
-#     meta = {
-#         external-node = "true"
-#         external-probe = "false"
-#     }
+    name = "vault"
+    node = resource.consul_node.vault.name
+    port = 8200
+    meta = {
+        external-node = "true"
+        external-probe = "false"
+    }
 
-#     tags = ["v1"]
+    tags = ["v1"]
 
-#     check {
-#         check_id                          = "service:vault_api"
-#         name                              = "vault health"
-#         status = "passing"
-#         http                              = "https://vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud:8200/v1/sys/health"
-#         tls_skip_verify                   = true
-#         interval                          = "10s"
-#         timeout                           = "5s"
-#         deregister_critical_service_after = "30s"
+    check {
+        check_id                          = "service:vault_api"
+        name                              = "vault health"
+        status = "passing"
+        http                              = "https://vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud:8200/v1/sys/health"
+        tls_skip_verify                   = true
+        interval                          = "10s"
+        timeout                           = "5s"
+        deregister_critical_service_after = "30s"
 
-#     }
-# }
+    }
+}
