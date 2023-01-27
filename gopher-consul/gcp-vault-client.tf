71c6c4b34a3504b22826b64900a3023d243a2065
HashiCoop <cooper@hashicorp.com>
2023-01-27 15:01:10 -0600

    deployment-orgchart.yaml test

---
+++ gopher-consul/gcp-vault-client.tf
@@ -0,0 +1,20 @@
+# resource "kubernetes_namespace" "gcp_vault" {
+#     provider = kubernetes.gcp
+#   metadata {
+#     name = "vault"
+#   }
+# }
+
+resource "helm_release" "consul" {
+  provider  = helm.gcp
+  name      = "orgchart"
+#   namespace = kubernetes_namespace.consul.metadata[0].name
+
+#   repository = "https://helm.releases.hashicorp.com"
+#   chart      = "consul"
+
+  values = [
+    "${file("config/deployment-orgchart.yaml")}"
+  ]
+}
+
