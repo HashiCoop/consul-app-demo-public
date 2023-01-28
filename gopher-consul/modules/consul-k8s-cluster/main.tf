2b38d4737c7220a7f4fea45593c2328cca05a8e5
HashiCoop <cooper@hashicorp.com>
2023-01-28 17:28:36 -0600

    typo

--- gopher-consul/modules/consul-k8s-cluster/main.tf
+++ gopher-consul/modules/consul-k8s-cluster/main.tf
@@ -1,7 +1,7 @@
 provider "kubernetes" {
   host                   = var.KUBERNETES_AUTH.host
-  token                  = var.KUBERNETS_AUTH.token
-  cluster_ca_certificate = var.KUBERNETS_AUTH.cluster_ca_certificate
+  token                  = var.KUBERNETES_AUTH.token
+  cluster_ca_certificate = var.KUBERNETES_AUTH.cluster_ca_certificate
 }
 
 resource "kubernetes_namespace" "consul" {
@@ -13,8 +13,8 @@ resource "kubernetes_namespace" "consul" {
 provider "helm" {
   kubernetes {
     host                   = var.KUBERNETES_AUTH.host
-    token                  = var.KUBERNETS_AUTH.token
-    cluster_ca_certificate = var.KUBERNETS_AUTH.cluster_ca_certificate
+    token                  = var.KUBERNETES_AUTH.token
+    cluster_ca_certificate = var.KUBERNETES_AUTH.cluster_ca_certificate
   }
 }
 
