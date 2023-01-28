f365ee3ed9bf3dcf50283de683b98bc48be21e1a
HashiCoop <cooper@hashicorp.com>
2023-01-28 17:24:07 -0600

    can't loop providers

--- gopher-consul/peering.tf
+++ gopher-consul/peering.tf
@@ -16,9 +16,8 @@ provider "consul" {
   insecure_https = true
 }
 
-resource "consul_config_entry" "mesh" {
-  for_each = [provider.aws, provider.consul]
-  provider = each.value
+resource "consul_config_entry" "aws_mesh_gateway" {
+  provider = consul.aws
 
   name      = "mesh"
   kind      = "mesh"
@@ -30,16 +29,31 @@ resource "consul_config_entry" "mesh" {
   })
 }
 
-resource "consul_peering_token" "aws-gcp" {
-  provider  = consul.aws
-  peer_name = "gcp-cluster"
-}
-
-resource "consul_peering" "gcp-aws" {
+resource "consul_config_entry" "gcp_mesh_gateway" {
   provider = consul.gcp
 
-  peer_name     = "aws-cluster"
-  peering_token = consul_peering_token.aws-gcp.peering_token
+  name      = "mesh"
+  kind      = "mesh"
+  partition = "default"
+  namespace = "consul"
+
+  config_json = jsonencode({
+      PeerThroughMeshGateways = true
+  })
+}
+
+resource "consul_peering_token" "aws_gcp" {
+  provider  = consul.aws
+  peer_name = "gcp-us-central1-c"
+
+
+}
+
+resource "consul_peering" "aws_gcp" {
+  provider = consul.gcp
+
+  peer_name     = "aws-us-east-1"
+  peering_token = consul_peering_token.aws_gcp.peering_token
 
   depends_on = [
     kubernetes_manifest.gcp_mesh_gateway
