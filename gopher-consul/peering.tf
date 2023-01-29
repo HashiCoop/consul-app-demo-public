05d348839fe1d5bb5811941ae47fa2653d5ad588
HashiCoop <cooper@hashicorp.com>
2023-01-28 18:48:40 -0600

    try to fix peering notation before moving to crd

--- gopher-consul/peering.tf
+++ gopher-consul/peering.tf
@@ -1,3 +1,19 @@
+# provider "kubernetes" {
+#   alias = "aws"
+  
+#   host                   = data.aws_eks_cluster.cluster.endpoint
+#   token                  = data.aws_eks_cluster_auth.cluster.token
+#   cluster_ca_certificate = base64decode(data.aws_eks_cluster.cluster.certificate_authority.0.data)
+# }
+
+# provider "kubernetes" {
+#   alias = "gcp"
+  
+#   host  = "https://${data.google_container_cluster.cluster.endpoint}"
+#   token = data.google_client_config.provider.access_token
+#   cluster_ca_certificate = base64decode(data.google_container_cluster.cluster.master_auth[0].cluster_ca_certificate)
+# }
+
 provider "consul" {
   alias = "aws"
 
@@ -23,7 +39,9 @@ resource "consul_config_entry" "aws_mesh_gateway" {
   kind      = "mesh"
 
   config_json = jsonencode({
-      PeerThroughMeshGateways = true
+    peering = {
+      peerThroughMeshGateways = true
+    }
   })
 }
 
@@ -34,9 +52,24 @@ resource "consul_config_entry" "gcp_mesh_gateway" {
   kind      = "mesh"
 
   config_json = jsonencode({
-      PeerThroughMeshGateways = true
+    peering = {
+      peerThroughMeshGateways = true
+    }
   })
 }
+
+# resource "kubernetes_manifest" "aws_mesh_gateway" {
+#   provider = kubernetes.aws
+
+#   manifest = yamldecode(file("config/mesh-gw.yaml"))
+
+# }
+
+# resource "kubernetes_manifest" "gcp_mesh_gateway" {
+#   provider = kubernetes.gcp
+
+#   manifest = yamldecode(file("config/mesh-gw.yaml"))
+# }
 
 resource "consul_peering_token" "aws_gcp" {
   provider  = consul.aws
@@ -46,7 +79,6 @@ resource "consul_peering_token" "aws_gcp" {
   depends_on = [
         consul_config_entry.aws_mesh_gateway
   ]
-
 }
 
 resource "consul_peering" "aws_gcp" {
