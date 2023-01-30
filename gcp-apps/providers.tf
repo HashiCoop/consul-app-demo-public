ee44a1445a2950ce7658ba48907f391fda49e2ba
HashiCoop <cooper@hashicorp.com>
2023-01-30 12:49:35 -0600

    hardcode

--- gcp-apps/providers.tf
+++ gcp-apps/providers.tf
@@ -3,10 +3,15 @@ data "tfe_outputs" "consul" {
   workspace    = var.TFC_CONSUL_WORKSPACE
 }
 
+variable "GCP_CONSUL_TOKEN" {
+  type = string
+}
+
 provider "consul" {
   # address        = data.tfe_outputs.consul.values.gcp_consul_auth.address
   address = "https://34.68.143.78"
-  token          = data.tfe_outputs.consul.values.gcp_consul_auth.token
+  # token          = data.tfe_outputs.consul.values.gcp_consul_auth.token
+  token = var.GCP_CONSUL_TOKEN
   scheme         = "https"
   insecure_https = true
 }
