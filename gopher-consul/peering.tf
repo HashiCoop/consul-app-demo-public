f50aea023729b5713c90f462fa2c0515a69ec6f1
HashiCoop <cooper@hashicorp.com>
2023-01-29 23:11:08 -0600

    peer clusters

--- gopher-consul/peering.tf
+++ gopher-consul/peering.tf
@@ -14,60 +14,55 @@ variable "GCP_CONSUL_TOKEN" {
 provider "consul" {
   alias   = "gcp"
 
-  address        = "https://34.68.143.78"
-  # token          = module.gcp_consul.consul_bootstrap_acl_token
-    token          = var.GCP_CONSUL_TOKEN
+  address        = "https://${module.gcp_consul.consul_ui_endpoint.ip}"
+  token          = module.gcp_consul.consul_bootstrap_acl_token
   scheme         = "https"
   insecure_https = true
 }
 
-output "gcp_consul_host" {
-  value = "https://${module.gcp_consul.consul_ui_endpoint.ip}"
+resource "consul_config_entry" "aws_mesh_gateway" {
+  provider = consul.aws
+
+  name      = "mesh"
+  kind      = "mesh"
+
+  config_json = jsonencode({
+    Peering = {
+      PeerThroughMeshGateways = true
+    }
+  })
 }
 
-# resource "consul_config_entry" "aws_mesh_gateway" {
-#   provider = consul.aws
+resource "consul_config_entry" "gcp_mesh_gateway" {
+  provider = consul.gcp
 
-#   name      = "mesh"
-#   kind      = "mesh"
+  name      = "mesh"
+  kind      = "mesh"
 
-#   config_json = jsonencode({
-#     Peering = {
-#       PeerThroughMeshGateways = true
-#     }
-#   })
-# }
+  config_json = jsonencode({
+    Peering = {
+      PeerThroughMeshGateways = true
+    }
+  })
+}
 
-# resource "consul_config_entry" "gcp_mesh_gateway" {
-#   provider = consul.gcp
+resource "consul_peering_token" "aws_gcp" {
+  provider  = consul.aws
 
-#   name      = "mesh"
-#   kind      = "mesh"
+  peer_name = "gcp-us-central1-c"
 
-#   config_json = jsonencode({
-#     Peering = {
-#       PeerThroughMeshGateways = true
-#     }
-#   })
-# }
+  depends_on = [
+        consul_config_entry.aws_mesh_gateway
+  ]
+}
 
-# resource "consul_peering_token" "aws_gcp" {
-#   provider  = consul.aws
+resource "consul_peering" "aws_gcp" {
+  provider = consul.gcp
 
-#   peer_name = "gcp-us-central1-c"
+  peer_name     = "aws-us-east-1"
+  peering_token = consul_peering_token.aws_gcp.peering_token
 
-#   depends_on = [
-#         consul_config_entry.aws_mesh_gateway
-#   ]
-# }
-
-# resource "consul_peering" "aws_gcp" {
-#   provider = consul.gcp
-
-#   peer_name     = "aws-us-east-1"
-#   peering_token = consul_peering_token.aws_gcp.peering_token
-
-#   depends_on = [
-#     consul_config_entry.gcp_mesh_gateway
-#   ]
-# }
+  depends_on = [
+    consul_config_entry.gcp_mesh_gateway
+  ]
+}
