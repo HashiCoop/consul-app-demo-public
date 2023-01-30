fbffb15d83ae32e5e5df290d6c8b92e8daa97dc3
HashiCoop <cooper@hashicorp.com>
2023-01-30 00:23:02 -0600

    remove https

--- gcp-apps/vars.tf
+++ gcp-apps/vars.tf
@@ -15,7 +15,7 @@ variable "VAULT_NAME" {
 
 variable "VAULT_EXTERNAL_ADDRESS" {
   type    = string
-  default = "https://vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud"
+  default = "vault-cluster-public-vault-fcbc1a73.d087f7bf.z1.hashicorp.cloud"
 }
 
 variable "VAULT_PORT" {
