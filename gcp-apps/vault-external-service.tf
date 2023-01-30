1ca85a1d014356c38f23431f7c280ea2656f0c11
HashiCoop <cooper@hashicorp.com>
2023-01-30 09:01:19 -0600

    remove vault service from tf config

--- gcp-apps/vault-external-service.tf
+++ gcp-apps/vault-external-service.tf
@@ -3,28 +3,27 @@ resource "consul_node" "vault" {
   address = var.VAULT_EXTERNAL_ADDRESS
 }
 
-resource "consul_service" "vault" {
-  name = resource.consul_node.vault.name
-  node = resource.consul_node.vault.name
-  port = var.VAULT_PORT
+# resource "consul_service" "vault" {
+#   name = resource.consul_node.vault.name
+#   node = resource.consul_node.vault.name
+#   port = var.VAULT_PORT
 
-  meta = {
-    external-node  = "true"
-    external-probe = "true"
-  }
+#   meta = {
+#     external-node  = "true"
+#     external-probe = "true"
+#   }
 
-  check {
-    check_id                          = "service:${var.VAULT_NAME}-api"
-    name                              = "${var.VAULT_NAME}-health"
-    status                            = "passing"
-    http                              = "https://${var.VAULT_EXTERNAL_ADDRESS}:${var.VAULT_PORT}/v1/sys/health"
-    tls_skip_verify                   = true
-    interval                          = "10s"
-    timeout                           = "5s"
-    deregister_critical_service_after = "30s"
-
-  }
-}
+#   check {
+#     check_id                          = "service:${var.VAULT_NAME}-api"
+#     name                              = "${var.VAULT_NAME}-health"
+#     status                            = "passing"
+#     http                              = "https://${var.VAULT_EXTERNAL_ADDRESS}:${var.VAULT_PORT}/v1/sys/health"
+#     tls_skip_verify                   = true
+#     interval                          = "10s"
+#     timeout                           = "5s"
+#     deregister_critical_service_after = "30s"
+#   }
+# }
 
 resource "consul_config_entry" "terminating_gateway" {
   name = "${consul_service.vault.name}-terminating-gateway"
