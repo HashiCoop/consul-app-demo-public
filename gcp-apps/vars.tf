2d5bc70bd8c1bc7b84a249c374d45721cd2cf11c
HashiCoop <cooper@hashicorp.com>
2023-01-30 18:08:30 -0600

    update addr

--- gcp-apps/vars.tf
+++ gcp-apps/vars.tf
@@ -15,7 +15,7 @@ variable "VAULT_NAME" {
 
 variable "VAULT_EXTERNAL_ADDRESS" {
   type    = string
-  default = "http://35.168.1.122:8200/v1/sys/health"
+  default = "35.168.1.122"
 }
 
 variable "VAULT_PORT" {
