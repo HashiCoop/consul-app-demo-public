aa5b8dd24c36d3e6d38376970f63acf673b8a36c
HashiCoop <cooper@hashicorp.com>
2023-01-31 17:24:46 -0600

    add vault external gateway

--- gcp-apps/providers.tf
+++ gcp-apps/providers.tf
@@ -3,15 +3,9 @@ data "tfe_outputs" "consul" {
   workspace    = var.TFC_CONSUL_WORKSPACE
 }
 
-variable "GCP_CONSUL_TOKEN" {
-  type = string
-}
-
 provider "consul" {
-  # address        = data.tfe_outputs.consul.values.gcp_consul_auth.address
-  address = "https://34.68.143.78"
-  # token          = data.tfe_outputs.consul.values.gcp_consul_auth.token
-  token = var.GCP_CONSUL_TOKEN
+  address        = data.tfe_outputs.consul.values.gcp_consul_auth.address
+  token          = data.tfe_outputs.consul.values.gcp_consul_auth.token
   scheme         = "https"
   insecure_https = true
 }
